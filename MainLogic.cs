8bbfcb90a1ca9149fa06fdca54d9c1bc3453b2eb
Extaz <nikitqalevioffanov@gmail.com>
2023-04-26 20:08:19 +0300

    Добавьте файлы проекта.

---
+++ MainLogic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfSea
+{
+    internal class MainLogic
+    {
+        static char[,] _playerField = new char[10, 10];
+        static char[,] _computerField = new char[10, 10];
+
+        static void GameCycle()
+        {
+            FillField(_playerField);
+            FillField(_computerField);
+        }
+
+        internal static void FillField(char[,] field)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    field[i, j] = '.';
+                }
+            }
+        }
+
+
+
+
+
+        internal void GenerateShip(char[,] field)
+        {
+            (int fourDeck, int threeDeck, int doubleDeck, int singleDeck) ships = (1, 2, 3, 4);
+
+            for (int i = 0; i < ships.fourDeck; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ Ship.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleOfSea
+{
+
+    internal class Ship
+    {
+        int _x = 999;
+        int _y = 999;
+        static Random random = new Random();
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
+        public int VolumeOccupiedCells { get { return 0; } }
+
+        internal List<(int x, int y)> Decks = new List<(int x, int y)>();
+
+
+        internal static bool CheckBoundaryOfDeck(char[,] field, (int x, int y) coords)
+        {
+            for (int i = coords.x - 1; i <= coords.x + 1; i++)
+            {
+                for (int j = coords.y - 1; j <= coords.y + 1; j++)
+                {
+                    if (i >= 0 && j >= 0 && i < field.GetLength(0) && j < field.GetLength(1))
+                    {
+                        if (field[i, j] == field[coords.x, coords.y])
+                            continue;
+                        if (field[i, j] == '*')
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        internal bool CheckBoundaryOfShip(char[,] field, Ship ship)
+        {
+            foreach (var deck in ship.Decks)
+            {
+                if (!CheckBoundaryOfDeck(field, deck))
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        internal Ship(char[,] field,int numOfDecks)
+        {
+           
+            do
+            {
+                _x = random.Next(0, 10);
+                _y = random.Next(0, 10);
+            } while (CheckBoundaryOfShip(field, ship));
+        }
+    }
+}
